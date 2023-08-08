de8624b8227a451d4ac5d99ae666d9678412ab96
ElNeko42 <franson177@gmail.com>
2023-08-08 15:57:01 +0200

    funcionamiento

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  vidaJugador1: number = 20;
+  vidaJugador2: number = 20;
 
   constructor() {}
+
+  disminuirVida(jugador: number) {
+    if (jugador === 1) {
+      this.vidaJugador1--;
+    } else {
+      this.vidaJugador2--;
+    }
+    this.lifeChanged(jugador);
+  }
+
+  aumentarVida(jugador: number) {
+    if (jugador === 1) {
+      this.vidaJugador1++;
+    } else {
+      this.vidaJugador2++;
+    }
+    this.lifeChanged(jugador);
+  }
+
+  lifeChanged(player: number) {
+    if (player === 1) {
+        const player1Element = document.querySelector('.player1');
+        if (player1Element) {
+            player1Element.classList.add('life-changed');
+            setTimeout(() => {
+                player1Element.classList.remove('life-changed');
+            }, 300);
+        }
+    } else {
+        const player2Element = document.querySelector('.player2');
+        if (player2Element) {
+            player2Element.classList.add('life-changed');
+            setTimeout(() => {
+                player2Element.classList.remove('life-changed');
+            }, 300);
+        }
+    }
+}
 
 }
